355d8218b945d6b8af2cc14040a615a457dafc70
gracenansamba <47112847+gracenansamba@users.noreply.github.com>
2019-11-07 22:21:39 -0500

    Add files via upload

--- ijk.cpp
+++ ijk.cpp
@@ -7,17 +7,17 @@ int A [3][3] ={{1,2,3},{4,5,6},{7,8,9}};
 int B [3][3] ={{1,2,3},{4,5,6},{7,8,9}};
 int C [3][3] ={{0,0,0},{0,0,0},{0,0,0}};
 
+
 for (int i=0; i<3; i++){
 	printf("i= %d", i);printf("\n");
-	for (int j=0; j<3; j++){
-		printf("j= %d", j);printf("\n");
-		int temp = 0;
+	for (int k=0; k<3; k++){
+		printf("k= %d", k);printf("\n");
+		int temp = A[i][k]; //A is fixed , then B and C are columnwise
 		printf("temp= %d", temp);printf("\n");
-		for (int k=0; k<3; k++){
-			printf("k= %d", k);printf("\n");
-			temp += A[i][k] * B[k][j]; //C is fixed while A is rowWise and B is columnWise
-			C[i][j]=temp;
-			printf("Cij= %d", temp);printf("\n");
+		for (int j=0; j<3; j++){
+			printf("j= %d", j);printf("\n");
+			C[i][j] += B[k][j] * temp ; //C is updated rowwise 
+			printf("Cij= %d", C[i][j]);printf("\n");
 		}	
 	}
 }
@@ -30,4 +30,4 @@ for (int i=0; i<3; i++){
 		printf("\n");
 	}
 	return 0;
-}	
+}
