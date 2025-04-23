eeeb4ca0c419d34b57ac0de92f6bdfd2e40f83e1
Grace Nansamba <nansamba1@dane7.llnl.gov>
2025-04-23 16:02:48 -0700

    lets master c++

---
+++ C++/hello.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+int main(){
+	int num, num2;
+	char operand;
+	std::string name;
+	std::cout<<"Hello world!!"<<std::endl;
+	std::cout<<"enter your name"<<std::endl;
+	std::cin>>name;
+	std::cout<<"My name is: "<<name<<std::endl;
+
+	std::cout<<"Calculator"<<std::endl;
+	std::cout<<"enter a number"<<std::endl;
+	std::cin>>num, num2;
+	std::cout<<"enter another number"<<std::endl;
+	std::cin>>num2;
+	std::cout<<"what operation do you want? +,-,/,*"<<std::endl;
+
+	
+
+}
